ebde1173e685b3f66c294157905518ea39b76c5a
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-24 18:07:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,9 +3,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    sss = (10 * "s")
-   puts sss + string
+    sss = 10 * "s"
+    sss + string
   else
-  puts string
+    string
   end
 end
